411c2eeda2dbbb6f9802c530af096aa5cbd133cc
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-13 16:32:03 +0600

    Added comments

--- lesson_3/control_flow/src/main.rs
+++ lesson_3/control_flow/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+   let x = 5;
+
+   if x < 5 {
+       println!("x is less than 5");
+   } else if x == 5 {
+       println!("x is equal to 5");
+   } else {
+       println!("x is greater than 5");
+   }
+
+   let condition = true;
+   let y = if condition { 5 } else { 6 };
+
+   println!("The value of y is: {}", y);
+
+   loop {
+       println!("again!");
+   }
 }
