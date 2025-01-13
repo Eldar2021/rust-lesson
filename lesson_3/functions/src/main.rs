802f69cd4f8797cfd638096f0cec007ac8d8e9b2
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-13 15:57:50 +0600

    Added functions

--- lesson_3/functions/src/main.rs
+++ lesson_3/functions/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
     println!("Hello, world!");
+
+    some_function();
+
+    parameter_function(10);
+
+    parameters_function(10, 20);
+
+    // Function body
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+
+    let x = summation(10, 20);
+    println!("The value of x is: {}", x);
+}
+
+fn some_function() {
+    println!("some_function ");
+}
+
+fn parameter_function(x: i32) {
+    println!("parameter_function x is: {}", x);
+}
+
+fn parameters_function(x: i32, y: i32) {
+    println!("parameters_function x is: {}, y is: {}", x, y);
+}
+
+fn summation(x: i32, y: i32) -> i32 {
+    x + y
 }
