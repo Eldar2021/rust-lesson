09ccc36d1d40b4c5baad4aad46f3eaa799faf281
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-13 16:02:37 +0600

    Added comments

--- lesson_3/comments/src/main.rs
+++ lesson_3/comments/src/main.rs
@@ -1,3 +1,17 @@
+
+// This is a main function
+// It is the entry point of the program
+// It is called when the program is run
 fn main() {
     println!("Hello, world!");
+}
+
+
+fn some_function() {
+    // This is a printing function
+    println!("some_function ");
+}
+
+fn parameter_function(x: i32) {
+    println!("parameter_function x is: {}", x); // This is a x parameter
 }
