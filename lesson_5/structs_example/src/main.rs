fb94b186e00e71bbc1200999ba99a4181ba813a0
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-15 16:15:34 +0600

    Added lesson 5

--- lesson_5/structs_example/src/main.rs
+++ lesson_5/structs_example/src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
     println!("Hello, world!");
+    exmaple_with_function();
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn exmaple_with_function() {
+    // v1
+    let width = 10;
+    let height = 20;
+    println!("Area v1: {}", calculate_area_v1(width, height));
+
+    // v2
+    let rect1 = (10, 20);
+    println!("Area v2: {}", calculate_area_v2(rect1));
+
+    // v3
+    let rect2 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    println!("Area v3: {}", calculate_area_v3(rect2));
+    let rect3 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    println!("rect3 is {rect3:?}");
+    println!("rect3 is {rect3:#?}");
+}
+
+fn calculate_area_v1(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn calculate_area_v2(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
+}
+
+fn calculate_area_v3(rect: Rectangle) -> u32 {
+    rect.width * rect.height
 }
--- lesson_5/structs_init/src/main.rs
+++ lesson_5/structs_init/src/main.rs
@@ -1,3 +1,71 @@
+struct User {
+    active: bool,
+    user_name: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+struct Color(i32, i32, i32);
+
 fn main() {
-    println!("Hello, world!");
+    // Struct
+    let mut user_1 = User {
+        active: true,
+        user_name: String::from("John"),
+        email: String::from("j@j.com"),
+        sign_in_count: 1,
+    };
+
+    println!("Hello, my name is {}!", user_1.user_name);
+    println!("Hello, my email is {}!", user_1.email);
+    println!("Hello, my sign_in_count is {}!", user_1.sign_in_count);
+    println!("Hello, my active is {}!", user_1.active);
+
+    user_1.email = String::from("jhone@gmail.com");
+    println!("Hi! Ivhanged my email to {}!", user_1.email);
+
+    let user_2 = build_user(String::from("mike@gmail.com"), String::from("Mike"));
+
+    println!("Hello, my name is {}!", user_2.user_name);
+    println!("Hello, my email is {}!", user_2.email);
+    println!("Hello, my sign_in_count is {}!", user_2.sign_in_count);
+    println!("Hello, my active is {}!", user_2.active);
+
+    let user_3 = User {
+        active: user_2.active,
+        user_name: String::from("Sam"),
+        email: String::from("sma@gmail.com"),
+        sign_in_count: user_2.sign_in_count,
+    };
+
+    println!("Hello, my name is {}!", user_3.user_name);
+
+    let user_4 = User {
+        email: String::from("j@j3.com"),
+        ..user_3
+    };
+
+    println!("Hello, my email is {}!", user_4.email);
+
+    // Tuple Struct
+    let black = Color(1, 2, 3);
+
+    println!("Balck first {}!", black.0);
+    println!("Balck second {}!", black.1);
+    println!("Balck third {}!", black.2);
+    // println!("Balck fourth {}!", black.3); // Gecersiz index
+
+    // Unit Struct
+    struct AlwaysEqual;
+
+    let subject = AlwaysEqual;
+}
+
+fn build_user(email: String, user_name: String) -> User {
+    User {
+        active: true,
+        user_name,
+        email,
+        sign_in_count: 1,
+    }
 }
