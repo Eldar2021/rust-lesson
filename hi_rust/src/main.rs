e165e0722867b96d90a2e67cac3744cf874d157b
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-02-12 23:53:10 +0600

    created hi rust server

---
+++ hi_rust/src/main.rs
@@ -0,0 +1,40 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    str, thread,
+    time::Duration,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, file_name) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", _path("hello.html")),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", _path("sleep.html"))
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", _path("404.html")),
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn _path(file: &str) -> String {
+    format!("/Users/eldiiaralmazbek/Desktop/rust/projects/hi_rust/assets/{file}")
+}
